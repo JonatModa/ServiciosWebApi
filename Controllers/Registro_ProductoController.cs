637f090ccdc776ac3a9a7f792c4732fb89064338
JONAT hernandez <jonat.her@hotmail.com>
2023-08-02 13:07:40 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/Registro_ProductoController.cs
@@ -0,0 +1,28 @@
+﻿using ServiciosWeb.Datos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class Registro_ProductoController : ApiController
+    {
+        LibreriaConnection BD = new LibreriaConnection();
+        [HttpGet]
+        public IEnumerable<Registro_Producto> Get()//viene del serviciosWeb.Datos
+        {
+            var listado = BD.Registro_Producto.ToList();
+            return listado;
+        }
+
+        [HttpGet]
+        public Registro_Producto Get(int Id_Producto)//viene del serviciosWeb.Datos
+        {
+            var listado = BD.Registro_Producto.FirstOrDefault(x => x.Id_Producto == Id_Producto);
+            return listado;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ServiciosWeb.WebApi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiciosWeb.WebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("2fd416c2-5259-47af-a8d7-f07cfc9c86b2")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
